c96a3bd64f62aa4177ff21610cff7414be076eb9
Kehinde Ayeyemi <kenvik446@gmail.com>
2022-07-08 05:24:34 +0000

    upgrade

--- file2.py
+++ file2.py
@@ -6,49 +6,64 @@ import pprint
 
 
 url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
-city_name = input("Enter the name of city: ")
-date = input("Enter the DATE (YYYY-MM-DD): ")
-
-querystring = {"q":city_name,"dt":date}
 
 headers = {
 	"X-RapidAPI-Key": "4386b895d6mshc4e43be45e4ac39p1385f8jsnf87c968c7233",
 	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
 }
+city_name = input("Enter the name of city: ")
+date = input("Enter the DATE (YYYY-MM-DD): ")
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-response2 = response.json()
-name = response2['location']['name']
-country = response2['location']['country']
-weather_condition = response2['current']['condition']['text']
-temp_f = response2['current']['temp_f']
-temp_c = response2['current']['temp_c']
-humidity = response2['current']['humidity']
-wind = response2['current']['wind_mph']
+querystring = {"q":city_name,"dt":date}
 
-report ={'name':name,
-       'country':country,
-	   'weather_condition':weather_condition,
-	   'temp_f':temp_f,
-	   'temp_c':temp_c,
-	   'humidity':humidity,
-	   'wind':wind
-      }
+def get_report(name, country, weather_condition, temp_f, temp_c, humidity,wind):
+    dict ={
+        'name':name,
+        'country':country,
+        'weather_condition':weather_condition,
+        'temp_f':temp_f,
+        'temp_c':temp_c,
+        'humidity':humidity,
+        'wind':wind
+        }
+    return dict
 
-data = pd.DataFrame(report ,index =[0])
-print(data)
+def get_forecast(city_name, date):
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response2 = response.json()
+
+    name = response2['location']['name']
+    country = response2['location']['country']
+    weather_condition = response2['current']['condition']['text']
+    temp_f = response2['current']['temp_f']
+    temp_c = response2['current']['temp_c']
+    humidity = response2['current']['humidity']
+    wind = response2['current']['wind_mph']
+
+    return get_report(name, country, weather_condition, temp_f, temp_c, humidity,wind)
+
+
+
+forecast= get_forecast(city_name, date)
+data = pd.DataFrame(forecast,index =[0])
 tmp = (data.loc[0].at["temp_f"])
 weather_cond = (data.loc[0]. at["weather_condition"])
 engine = db.create_engine('sqlite:///data.db')
 data.to_sql('data', con=engine, if_exists='replace', index=False)
 queryResult = engine.execute("SELECT * FROM data;").fetchall()
+print(pd.DataFrame(queryResult))
 
-#print(pd.DataFrame(queryResult))
-if tmp >=85:
-    print ("\nOnly 3 part time employees need to work today because the temperature is " + str(tmp) + "°f and it is " + str(weather_cond))
-elif (tmp <= 85 and tmp >= 75):
-    print("\n10 part employees need to work today because the temperature is " + str(tmp) + "°f and it is " + str(weather_cond))
-elif (tmp <= 75 and tmp >= 70):
-	print("\n5 part time employees need  to work today because the temperature is " + str(tmp) + "°f and it is " + str(weather_cond))
-else:
-	print("\nno part time employees need for  today because the temperature is " + str(tmp) + "°f and it is " + str(weather_cond))
+
+def num_of_employees(tmp):
+
+    if tmp >=85:
+        return 3 
+    elif (tmp <= 85 and tmp >= 75):
+        return 10
+    elif (tmp <= 75 and tmp >= 70):
+        return 5
+    else:
+        return 0
+
+employees = num_of_employees(tmp)
+print("\n" + str(employees) + " part time employees need to work today because the temperature is " + str(tmp) + "°f and it is " + str(weather_cond))
